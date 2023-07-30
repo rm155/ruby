61ad10ee58b761e4d9bd12de4037f627a11c9913
rm155 <rohitmenon@verizon.net>
2023-07-30 11:49:54 -0400

    Avoid marking global_tbl from local GCs of non-main Ractors

--- gc.c
+++ gc.c
@@ -8330,8 +8330,10 @@ gc_mark_roots(rb_objspace_t *objspace, const char **categoryp)
     MARK_CHECKPOINT("end_proc");
     rb_mark_end_proc(objspace);
 
-    MARK_CHECKPOINT("global_tbl");
-    rb_gc_mark_global_tbl();
+    if (!rb_multi_ractor_p() || objspace == vm->ractor.main_ractor || objspace->flags.during_global_gc) {
+	MARK_CHECKPOINT("global_tbl");
+	rb_gc_mark_global_tbl();
+    }
 
     rb_global_space_t *global_space = &rb_global_space;
 
