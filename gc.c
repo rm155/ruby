00d95414d7ee66685d781812e2a2fdc98e3f24e7
rm155 <rohitmenon@verizon.net>
2023-08-09 13:00:52 -0400

    Remove superfluous condition for global-table marking

--- gc.c
+++ gc.c
@@ -8379,7 +8379,7 @@ gc_mark_roots(rb_objspace_t *objspace, const char **categoryp)
     MARK_CHECKPOINT("end_proc");
     rb_mark_end_proc(objspace);
 
-    if (!rb_multi_ractor_p() || objspace == vm->objspace) {
+    if (objspace == vm->objspace) {
 	MARK_CHECKPOINT("global_tbl");
 	rb_gc_mark_global_tbl();
     }
